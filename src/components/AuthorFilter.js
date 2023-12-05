f7fe862121774e1bcc54345644884b13443ee0c2
PeppeRubino <121513779+PeppeRubino@users.noreply.github.com>
2023-12-05 19:35:04 +0100

    building fix 1
    useEffect fix 1

--- src/components/AuthorFilter.js
+++ src/components/AuthorFilter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import authorsData from '../JSON/authorsData.json';
 
 const AuthorFilter = ({ onFilterAuthor }) => {
