96c8d2e5063b1b7781189cdac8cfeabe08edcf4f
PeppeRubino <121513779+PeppeRubino@users.noreply.github.com>
2023-12-05 07:51:47 +0100

    11
    11

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -80,7 +80,7 @@ const Timeline = ({ selectedYear, selectedAuthor }) => {
   const handleMouseMove = useCallback((event) => {
     if (DRAG_START_X !== null) {
       const delta = event.clientX - DRAG_START_X;
-      setScrollX((prevScrollX) => (prevScrollX || 0) + delta);
+      setScrollX((prevScrollX) => (prevScrollX !== undefined ? prevScrollX : 0) + delta);
       setDragStartX(event.clientX);
     }
   }, [DRAG_START_X, prevScrollX]);
